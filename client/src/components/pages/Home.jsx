1ddca0af55eee494528c912ad5baec514421f51c
Christine Wu <christinew7125@gmail.com>
2025-01-19 14:56:00 -0500

    added more stuff to Home page

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,14 +1,30 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-// import { GoogleLogin, googleLogout } from "@react-oauth/google";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
 import "../../utilities.css";
 import "./Home.css";
-// import { UserContext } from "../context/UserContext";
+import { UserContext } from "../context/UserContext";
 
 const Home = () => {
+  const { userId, handleLogin, handleLogout } = useContext(UserContext);
+
   return (
     <div className="Home-container">
+      {userId ? (
+        <button
+          onClick={() => {
+            googleLogout();
+            handleLogout();
+          }}
+        >
+          Logout
+        </button>
+      ) : (
+        <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
+      )}
+
+      {/* Title and links */}
       <div className="Home-title">Branch & Bite @ MIT</div>
       <div className="Home-linkContainer u-inlineBlock">
         <Link to="/Maseeh" className="Home-link">
@@ -38,24 +54,3 @@ const Home = () => {
 };
 
 export default Home;
-
-{
-  /* <h1>Good luck on your project :)</h1>
-<h2> What you need to change in this skeleton</h2>
-<ul>
-  <li>
-    Change the Frontend CLIENT_ID (index.jsx) to your team's CLIENT_ID (obtain this at
-    http://weblab.is/clientid)
-  </li>
-  <li>Change the Server CLIENT_ID to the same CLIENT_ID (auth.js)</li>
-  <li>
-    Change the Database SRV (mongoConnectionURL) for Atlas (server.js). You got this in the
-    MongoDB setup.
-  </li>
-  <li>Change the Database Name for MongoDB to whatever you put in the SRV (server.js)</li>
-</ul>
-<h2>How to go from this skeleton to our actual app</h2>
-<a href="https://docs.google.com/document/d/110JdHAn3Wnp3_AyQLkqH2W8h5oby7OVsYIeHYSiUzRs/edit?usp=sharing">
-  Check out this getting started guide
-</a> */
-}
