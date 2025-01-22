6a77fe07a86954540afd20ab66d3af1a02607ded
Christine Wu <christinew7125@gmail.com>
2025-01-22 00:58:55 -0500

    changed express-session to cookie-session

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ require("dotenv").config();
 //import libraries needed for the webserver to work!
 const http = require("http");
 const express = require("express"); // backend framework for our node server.
-const session = require("express-session"); // library that stores info about each connected user
+const session = require("cookie-session"); // library that stores info about each connected user
 const mongoose = require("mongoose"); // library to connect to MongoDB
 const path = require("path"); // provide utilities for working with file and directory paths
 
@@ -54,8 +54,6 @@ mongoose
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(`Error connecting to MongoDB: ${err}`));
-
-
 
 // create a new express server
 const app = express();
